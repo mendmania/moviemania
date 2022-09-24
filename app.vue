4bfe6772b8ed1064c03c3ec5dca08ff33939ec93
mendmania <mendmania@gmail.com>
2022-09-24 13:54:56 +0200

    init commit

---
+++ app.vue
@@ -0,0 +1,12 @@
+
+<script setup>
+const meni = 1;
+console.log(meni)
+</script>
+    
+<template>
+  <div>
+    <NuxtPage />
+  </div>
+</template>
+    
---
+++ nuxt.config.ts
@@ -0,0 +1,21 @@
+// https://v3.nuxtjs.org/api/configuration/nuxt.config
+export default defineNuxtConfig({
+    
+    // components: true,
+    css:[
+        '@/assets/style/main.scss'
+    ],
+    // runtimeConfig: {
+    //     public: {
+    //       baseURL: process.env.BASE_URL || 'https://api.example.com/',
+    //     },
+    // },
+    runtimeConfig: {
+        // The private keys which are only available within server-side
+        apiSecret: '123',
+        // Keys within public, will be also exposed to the client-side
+        public: {
+            apiBase: 'https://www.tvmaze.com/api/'
+        }
+      }
+})
