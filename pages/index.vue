0953c1a68d1860116dbdc793c802374ea6e3334f
mendmania <mendmania@gmail.com>
2022-09-27 23:31:18 +0200

    slider fixes., header footer added

--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,23 @@
 <script setup>
 getShowsTest();
 
-let hoverShowData = useState("hoverShowData");
-let cardHover = useState("cardHover");
-let right = useState("right");
-let left = useState("left");
-let isHovering = useState("isHovering");
+// let hoverShowData = useState("hoverShowData");
+// let cardHover = useState("cardHover");
+// let right = useState("right");
+// let left = useState("left");
+// let isHovering = useState("isHovering");
 </script>
 
 <template>
-  <SectionWrapper />
-  <CardHover :show="hoverShowData"
-             v-if="cardHover"
-             :left="left"
-             :right="right"
-             :isHovering="isHovering"
-             ref="child" />
+  <SectionContainer>
+    <SectionWrapper />
+    <!-- <CardHover :show="hoverShowData"
+               v-if="cardHover"
+               :left="left"
+               :right="right"
+               :isHovering="isHovering"
+               ref="child" /> -->
+  </SectionContainer>
+
+
 </template>
